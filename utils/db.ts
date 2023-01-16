5ffada90e2bf66c33f4a3b8714df7470cbc38821
MacDonald Ginyani <68953760+theorem23@users.noreply.github.com>
2023-01-16 12:40:31 +0200

    origin

---
+++ utils/db.ts
@@ -0,0 +1,21 @@
+import React from 'react'
+import mongoose from 'mongoose';
+import { exit } from 'process'
+
+const db =async () => {
+    try {
+      console.log("DB:", process.env.NEXT_PUBLIC_DB_STRING);
+      const conn = await mongoose.connect(process.env.NEXT_PUBLIC_DB_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "instalert",
+      });
+  
+      console.log(`MongoDB Connected`);
+    } catch (error) {
+      console.log(error);
+      exit(1);
+    }
+  };
+  
+export default db
